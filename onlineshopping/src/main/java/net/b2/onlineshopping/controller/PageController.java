a58c07cee4e99db02b8e81362d8d7a5fc93ddc28
Bojan Bozanovic <bozanovicb@gmail.com>
2020-07-13 13:36:32 +0200

    Page Controller Java create

--- onlineshopping/src/main/java/net/b2/onlineshopping/controller/PageController.java
+++ onlineshopping/src/main/java/net/b2/onlineshopping/controller/PageController.java
@@ -11,7 +11,7 @@ public class PageController {
 	public ModelAndView index() {
 
 		ModelAndView mv = new ModelAndView("page");
-
+        mv.addObject("greeting","Welcome to Spring Web MVC");
 		return mv;
 
 	}
