e535af60c0de12643f74c46c7313b08d287c3684
Bojan Bozanovic <bozanovicb@gmail.com>
2020-07-13 13:35:23 +0200

    Solving Version Mismatch Errors and Adding Spring Dependency Configuration

---
+++ onlineshopping/src/main/java/net/b2/onlineshopping/controller/PageController.java
@@ -0,0 +1,18 @@
+package net.b2.onlineshopping.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class PageController {
+
+	@RequestMapping(value = { "/", "/home", "/index" })
+	public ModelAndView index() {
+
+		ModelAndView mv = new ModelAndView("page");
+
+		return mv;
+
+	}
+}
